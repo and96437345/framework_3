94411ad9d0edc328dcd1bea7a730219b85a6cd0e
and96437345 <and96437345@yandex.ru>
2023-11-16 22:03:46 +0300

    Framework_3
    Angular

--- my-app/src/app/home-work/home-work.component.ts
+++ my-app/src/app/home-work/home-work.component.ts
@@ -7,11 +7,14 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule],
   templateUrl: './home-work.component.html',
   styleUrl: './home-work.component.css',
+  
 })
 export class HomeWorkComponent {
   currency1 = 10000;
   currency2 = 100;
   percent1 = 1.5;
   percent2 = 24;
-  isTrue = false;
+  isTrue = true;
+  students =["Andrey", "Alexey", "Anna", "Tigran", "Alexey", "Anton", "Artyom"];
+  day = 3;
 }
